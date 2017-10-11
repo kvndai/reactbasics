3b467c53ee49e862725b571f1ad080043bbe14c6
kvndai <kevin.t.dai@gmail.com>
2017-10-10 19:52:05 -0700

    synthetic events/constructor

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  inputChange(event) {
+    console.log(event.target.value)
+  }
+  render() {
+    return (
+      <header>
+        <div 
+          className="logo"
+          onClick={()=>console.log('click')}
+        >
+          Logo
+        </div>
+        <input 
+          onChange={this.inputChange}
+        />
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
+
 const App = () => {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
